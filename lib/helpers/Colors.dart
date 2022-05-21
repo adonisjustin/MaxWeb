fd67ea7bb3e9d20a216d87d8cb40251beefeb7b6
Adonis Justin <36498579+adonisjustin@users.noreply.github.com>
2022-05-21 09:21:22 +0100

    Add files via upload

---
+++ lib/helpers/Colors.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryColors = MaterialColor(
+  0xFF30475E,
+  <int, Color>{
+    50: primaryColor,
+    100: primaryColor,
+    200: primaryColor,
+    300: primaryColor,
+    400: primaryColor,
+    500: primaryColor,
+    600: primaryColor,
+    700: primaryColor,
+    800: primaryColor,
+    900: primaryColor,
+  },
+);
+
+const Color primaryColor = Color(0xFF004A55);
+const Color backgroundColorLightTheme = Color(0xFFF5F5F5);
+const Color backgroundColorDarkTheme = Color(0xFF303132);
+const Color accentColor = Color(0xFF2A727D);
+const Color splashBackColor = Color(0xFFFFFFFF);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Color(0xFF000000);
---
+++ lib/helpers/Constant.dart
@@ -0,0 +1,54 @@
+const appName = 'Afflibase Agent';
+
+const String appbartitle = 'Afflibase Agent';
+const String andoidPackageName = 'com.agent.affilbase';
+
+//change this url to set your URL in app
+const String webinitialUrl = 'https://agents.affilbase.com';
+const String firstTabUrl = 'https://agents.affilbase.com/campaigns';
+
+//keep local content of pages of setting screen
+const String aboutPageURL = 'https://affilbase.com/about-us';
+const String privacyPageURL = 'https://affilbase.com/privacy-policy';
+const String termsPageURL = 'https://affilbase.com/terms-conditions';
+
+//Change App id of android and IOS app
+const String androidAppId = andoidPackageName;
+
+const String iOSAppId = '';
+
+const String shareAppTitle = appName;
+const String shareiOSAppMessage = '';
+const String shareAndroidAppMessge =
+    'Download $appName App from this link : http://play.google.com/store/apps/details?id=$androidAppId';
+
+//To turn on/off ads
+const bool showInterstitialAds = false;
+const bool showBannerAds = false;
+
+//To turn on/off display of bottom navigation bar
+const bool showBottomNavigationBar = true;
+
+//To show/remove splash screen
+const bool showSplashScreen = true;
+
+//to enable/disable prtrait mode
+const bool enablePortraitMode = false;
+
+//To remove/display header/footer of website
+const bool hideHeader = true;
+const bool hideFooter = true;
+
+//Ids for interstitial Ad
+const androidInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+const iosInterstitialAdId = 'ca-app-pub-3940256099942544/4411468910';
+
+//Ids for banner Ad
+const androidBannerAdId = 'ca-app-pub-3940256099942544/6300978111';
+const iosBannerAdId = 'ca-app-pub-3940256099942544/2934735716';
+
+//icon to set when get firebase messages
+const String notificationIcon = '@mipmap/ic_launcher_squircle';
+
+//path to icons   *** don't change these values**
+const String iconPath = 'assets/icons/';
